a5f034afbf959f817fc05663aac4507c2939c71e
Rick Martinez <rick.martinez84@gmail.com>
2026-03-02 20:38:15 -0600

    Create 01_customer_year.sql

---
+++ project-2-sql-case-study/data/sql/01_customer_year.sql
@@ -0,0 +1,11 @@
+WITH customer_year AS (
+    SELECT 
+        CustomerID,
+        Segment,
+        Category,
+        STRFTIME('%Y', OrderDate) AS OrderYear,
+        SUM(Sales) AS YearlySales
+    FROM superstore
+    GROUP BY 
+        CustomerID, Segment, Category, STRFTIME('%Y', OrderDate)
+)
